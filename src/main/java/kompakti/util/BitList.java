ff441b9abda61f2837c4a59a303174aefdb9f2d4
Vikke Tiirola <vikke.tiirola@gmail.com>
2020-05-29 00:40:32 +0300

    Update BitList functionality and add unit tests for it

--- src/main/java/kompakti/util/BitList.java
+++ src/main/java/kompakti/util/BitList.java
@@ -1,24 +1,118 @@
 package kompakti.util;
 
-public class BitArray {
+public class BitList {
     private int[] bits;
-    private int pointerPosition;
-    private int arraySize = 2048;
+    private int itemCount;
+    private int arraySize;
 
-    public BitArray() {
+    public BitList() {
+        arraySize = 2048;
         bits = new int[arraySize];
-        pointerPosition = 0;
+        itemCount = 0;
     }
 
-    public BitArray(int arraySize) {
+    public BitList(int arraySize) {
         this.arraySize = arraySize;
         bits = new int[arraySize];
-        pointerPosition = 0;
+        itemCount = 0;
     }
 
-    public write(int value) {
-
+    public int size() {
+        return itemCount;
     }
 
+    public void add(int value) {
+        if (itemCount == arraySize - 1) {
+            increaseSize();
+        }
+        bits[itemCount] = value;
+        itemCount++;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= itemCount) {
+            throw new IndexOutOfBoundsException("BitList index out of bounds");
+        }
+        return bits[index];
+    }
+//    public void remove()
+
+
+    // Each item is presented as 12 bit in byte array.
+    public byte[] getListAsByteArray() {
+        int byteArraySize = (itemCount + (itemCount / 2)) + (itemCount % 2 == 0 ? 0 : 1);
+        byte[] byteArray = new byte[byteArraySize];
+
+        int byteCount = 0;
+        byte helperByte = 0;
+
+        int evenLeftMask = 4080;
+        int evenRightMask = 15;
+        int oddLeftMask = 3840;
+        int oddRightMask = 255;
+
+        for (int i = 0; i < itemCount; i++) {
+            if (i % 2 == 0) {
+                byte left = (byte) ((bits[i] & evenLeftMask) >> 4);
+                byte right = (byte) ((bits[i] & evenRightMask) << 4);
+                byteArray[byteCount] = left;
+                helperByte = right;
+                byteCount++;
+
+                if (i == itemCount - 1) {
+                    // If last item
+                    byteArray[byteCount] = right;
+                }
+            } else {
+                byte left = (byte) (helperByte + ((bits[i] & oddLeftMask) >> 8));
+                byte right = (byte) (bits[i] & oddRightMask);
+                byteArray[byteCount] = left;
+                byteArray[byteCount + 1] = right;
+
+                byteCount += 2;
+            }
+        }
+
+
+        return byteArray;
+    }
+//    public byte[] getListAsByteArray(int maxValue) throws Exception {
+//        if (getMaxItem() > maxValue) {
+//            throw new Exception("BitList contains too big values for byte[]");
+//        }
+//
+//        byte[] byteBuffer = new byte[2];
+//        for (int i = 0; i < ; i++) {
+//
+//        }
+//
+//    }
+
+
+    public int getMaxItem() {
+        int max = 0;
+        for (int i = 0; i < itemCount; i++) {
+            max = max > bits[i] ? max : bits[i];
+        }
+        return max;
+    }
+
+    private void increaseSize() {
+        int[] newBits = new int[arraySize + arraySize / 2];
+        for (int i = 0; i < arraySize; i++) {
+            newBits[i] = bits[i];
+        }
+        bits = newBits;
+        arraySize += arraySize / 2;
+    }
+
+    private void decreaseSize() {
+        int[] newBits = new int[arraySize / 2];
+        for (int i = 0; i < (arraySize / 4); i++) {
+            newBits[i] = bits[i];
+        }
+        bits = newBits;
+        arraySize /= 2;
+    }
 
 }
--- src/test/java/kompakti/util/BitListTest.java
+++ src/test/java/kompakti/util/BitListTest.java
@@ -1,7 +1,49 @@
 package kompakti.util;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
+
 public class BitListTest {
+    private BitList bitList;
+
+    @Before
+    public void setUp() {
+        bitList = new BitList();
+    }
+
+    @Test
+    public void correctSizeIsCreated() {
+        BitList bitListWithSize = new BitList(2048);
+        assertEquals(0, bitList.size());
+        assertEquals(0, bitListWithSize.size());
+    }
+
+    @Test
+    public void oneItemCorrectlyAdded() {
+        bitList.add(10);
+        assertEquals(10, bitList.get(0));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void negativeIndexThrowsException() {
+        bitList.get(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void notExistingIndexThrowsException() {
+        bitList.get(0);
+    }
+
+    @Test
+    public void listSizeCorrectlyIncreased() {
+        for (int i = 0; i < 2049; i++) {
+            bitList.add(i);
+        }
+        assertEquals(2048, bitList.get(2048));
+    }
+
 
 }
