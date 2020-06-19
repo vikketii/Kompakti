3b89070dcb94f59cde085d6a937535296b966d4e
Vikke Tiirola <vikke.tiirola@gmail.com>
2020-06-19 16:00:43 +0300

    Update cli usage

--- src/main/java/kompakti/Main.java
+++ src/main/java/kompakti/Main.java
@@ -29,8 +29,14 @@ public class Main {
             case "--lzw":
                 lzwCompression(bytes, fr, filename);
                 break;
+            case "--lzw-decomp":
+                lzwDecompression(bytes, fr, filename);
+                break;
             case "--huffman":
                 huffmanCompression(bytes, fr, filename);
+                break;
+            case "--huffman-decomp":
+                huffmanDecompression(bytes, fr, filename);
                 break;
             default:
                 System.err.println("Invalid argument " + flag);
@@ -42,12 +48,23 @@ public class Main {
     private static void lzwCompression(byte[] bytes, FileReader fr, String filename) {
         LZW lzw = new LZW();
         byte[] compressed = lzw.compress(bytes);
-        byte[] decompressed = lzw.decompress(compressed);
+//        byte[] decompressed = lzw.decompress(compressed);
 
         try {
             fr.writeFile(filename + ".lzw", compressed);
-            fr.writeFile(filename + ".lzwdecomp", decompressed);
+//            fr.writeFile(filename + ".lzwdecomp", decompressed);
             presentBytes(bytes, compressed);
+        } catch (Exception e) {
+            System.out.println("Give filename as argument");
+        }
+    }
+
+    private static void lzwDecompression(byte[] bytes, FileReader fr, String filename) {
+        LZW lzw = new LZW();
+        byte[] decompressed = lzw.decompress(bytes);
+
+        try {
+            fr.writeFile(filename + ".lzwdecomp", decompressed);
         } catch (Exception e) {
             System.out.println("Give filename as argument");
         }
@@ -67,11 +84,25 @@ public class Main {
         }
     }
 
+    private static void huffmanDecompression(byte[] bytes, FileReader fr, String filename) {
+        Huffman huff = new Huffman();
+        byte[] decompressed = huff.decompress(bytes);
+
+        try {
+            fr.writeFile(filename + ".huffdecomp", decompressed);
+        } catch (Exception e) {
+            System.out.println("Give filename as argument");
+        }
+    }
+
 
     private static void help() {
         System.out.println("Usage: [OPTION] [FILE]");
         System.out.println();
         System.out.println("--lzw: makes LZW compression of given file and saves it as .lzw file");
+        System.out.println("--lzw-decomp: makes LZW decompression of given file and saves it as .lzwdecomp file");
+        System.out.println("--huffman: makes Huffman compression of given file and saves it as .huff file");
+        System.out.println("--huffman-decomp: makes Huffman decompression of given file and saves it as .huffdecomp file");
         System.out.println("--help: prints this help message");
     }
 
