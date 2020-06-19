223bcb67c1a1f341d953ace3f434eae325080279
Vikke Tiirola <vikke.tiirola@gmail.com>
2020-06-19 15:54:59 +0300

    Rename BitListTest to generic ArrayListTest

--- src/test/java/kompakti/util/ArrayListTest.java
+++ src/test/java/kompakti/util/ArrayListTest.java
@@ -6,43 +6,43 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
-public class BitListTest {
-    private BitList bitList;
+public class ArrayListTest {
+    private ArrayList arrayList;
 
     @Before
     public void setUp() {
-        bitList = new BitList();
+        arrayList = new ArrayList();
     }
 
     @Test
     public void correctSizeIsCreated() {
-        BitList bitListWithSize = new BitList(2048);
-        assertEquals(0, bitList.size());
-        assertEquals(0, bitListWithSize.size());
+        ArrayList arrayListWithSize = new ArrayList(2048);
+        assertEquals(0, arrayList.size());
+        assertEquals(0, arrayListWithSize.size());
     }
 
     @Test
     public void oneItemCorrectlyAdded() {
-        bitList.add(10);
-        assertEquals(10, bitList.get(0));
+        arrayList.add(10);
+        assertEquals(10, arrayList.get(0));
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void negativeIndexThrowsException() {
-        bitList.get(-1);
+        arrayList.get(-1);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void notExistingIndexThrowsException() {
-        bitList.get(0);
+        arrayList.get(0);
     }
 
     @Test
     public void listSizeCorrectlyIncreased() {
         for (int i = 0; i < 2049; i++) {
-            bitList.add(i);
+            arrayList.add(i);
         }
-        assertEquals(2048, bitList.get(2048));
+        assertEquals(2048, arrayList.get(2048));
     }
 
 
