4ab6a8daa19a69441fb2d9b705210e6faa2485a6
Vikke Tiirola <vikke.tiirola@gmail.com>
2020-06-27 19:32:30 +0300

    Add testing for Huffman

--- src/test/java/kompakti/compression/HuffmanTest.java
+++ src/test/java/kompakti/compression/HuffmanTest.java
@@ -1,9 +1,39 @@
 package kompakti.compression;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
 
 public class HuffmanTest {
+    private Huffman huffman;
 
-    // How to do file that ends to 0?
+    @Before
+    public void setUp() {
+        huffman = new Huffman();
+    }
 
+    @Test
+    public void correctCompressionAndDecompressionOfSmallText() {
+        String original = "Tama on testi ABABABööABööAB";
+        byte[] compressed = huffman.compress(original.getBytes());
+        byte[] decompressed = huffman.decompress(compressed);
+        assertArrayEquals(original.getBytes(), decompressed);
+    }
+
+    @Test
+    public void correctCompressionAndDecompressionOfLongText() throws IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("alice29.txt");
+        byte[] alice = inputStream.readAllBytes();
+        inputStream.close();
+
+        assertEquals(152089, alice.length);
+        byte[] compressed = huffman.compress(alice);
+        byte[] decompressed = huffman.decompress(compressed);
+        assertArrayEquals(alice, decompressed);
+    }
 }
